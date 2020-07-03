412dad50fef0818c99919cf1221655f07e910edd
Neelkumar Amin <neelkumar.amin@outlook.com>
2020-07-02 21:19:34 -0400

    main: created main method to run spring boot application

---
+++ src/main/java/com/n33l4m1n/github/springbootapi/Application.java
@@ -0,0 +1,12 @@
+package com.n33l4m1n.github.springbootapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
